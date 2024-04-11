35f1b1ca8f2979e05609ab14b7eb925d5b4c4f32
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-11 21:56:29 +0200

    Initial commit

---
+++ src/main/java/com/artemnizhnyk/springaipractice/SpringAiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.springaipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artemnizhnyk/springaipractice/SpringAiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.springaipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
